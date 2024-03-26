d164baad094f641c3af1b6b20f1e21babe8dfd08
nurfikaalfianingrum <160575631+nurfikaalfianingrum@users.noreply.github.com>
2024-03-26 20:27:16 +0700

    LingkaranMain.java

---
+++ Tugas 4/modul 3/LingkaranMain.java
@@ -0,0 +1,12 @@
+public class LIngkaranMain {
+        public static void main(String[] args){
+        lingkaran l = new lingkaran(3);
+        l.setTinggi(10);
+        l.displayMessage();
+        lingkaran l2 = new lingkaran(4, 10);
+        l2.displayMessage();
+
+        lingkaran lString = new lingkaran(5, 12);
+        lString.displayMessage();
+        }
+        }
