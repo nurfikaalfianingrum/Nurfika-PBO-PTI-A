d2eb4b54fa5cf02a0e4cb7027033cb8e45725ff0
nurfikaalfianingrum <160575631+nurfikaalfianingrum@users.noreply.github.com>
2024-03-26 20:26:06 +0700

    Lingkaran.java

---
+++ Tugas 4/modul 3/Lingkaran.java
@@ -0,0 +1,51 @@
+public class lingkaran {
+    int alas, tinggi;
+    
+    public lingkaran(int alas) {
+        this.alas = alas;
+    }
+    
+    public lingkaran(int alas, int tinggi) {
+        this.alas = alas;
+        this.tinggi = tinggi;
+    }
+    
+    public lingkaran(String alas, String tinggi) {
+        this.alas = parseToInt(alas);
+        this.tinggi = parseToInt(tinggi);
+    }
+    
+    public void setAlas(int alas) {
+        this.alas = alas;
+    }
+    
+    public void setTinggi(int tinggi) {
+        this.tinggi = tinggi;
+    }
+    
+    public int getAlas() {
+        return alas;
+    }
+    
+    public int getTinggi() {
+        return tinggi;
+    }
+    
+    public double hitungLuas() {
+        double hasil = (double) (getTinggi() * getAlas()) / 2;
+        return hasil;
+    }
+    
+    public void displayMessage() {
+        System.out.println("Hitung Luas : " + hitungLuas());
+    }
+    
+    private int parseToInt(String str) {
+        try {
+            return Integer.parseInt(str);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Cannot parse " + str + " to integer.");
+            return 0; 
+        }
+    }
+}
