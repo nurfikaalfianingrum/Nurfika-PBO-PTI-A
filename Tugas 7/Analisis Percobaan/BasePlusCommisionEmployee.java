192c631b72d9e7ae8bd1020a3647701b5f8a512b
nurfikaalfianingrum <160575631+nurfikaalfianingrum@users.noreply.github.com>
2024-05-08 19:32:54 +0700

    BasePlusCommisionEmployee.java

---
+++ Tugas 7/Analisis Percobaan/BasePlusCommisionEmployee.java
@@ -0,0 +1,21 @@
+import java.time.LocalDate;
+public class BasePlusCommissionEmployee extends CommissionEmployee {
+    private double baseSalary;
+    public BasePlusCommissionEmployee(String name, String noKTP, double sales, double rate, double salary) {
+        super(name, noKTP, sales, rate);
+        setBaseSalary(salary);
+    }
+    public void setBaseSalary(double salary) {
+        baseSalary = salary;
+    }
+    public double getBaseSalary() {
+        return baseSalary;
+    }
+    public double earnings() {
+        return getBaseSalary() + super.earnings();
+    }
+    public String toString() {
+    return String.format("Base-Salaried " +
+    super.toString() + "\nbase salary " + getBaseSalary());
+    }
+}
