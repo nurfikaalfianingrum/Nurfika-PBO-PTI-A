516abacbd5d715282246d451dc3779e593c4d408
nurfikaalfianingrum <160575631+nurfikaalfianingrum@users.noreply.github.com>
2024-05-14 20:22:40 +0700

    Pegawai.java

---
+++ Tugas 8/Praktikum/Pegawai.java
@@ -0,0 +1,25 @@
+abstract class Pegawai {
+    private String nama;
+    private String noKTP;
+    
+    public Pegawai(String nama, String noKTP) {
+        this.nama = nama;
+        this.noKTP = noKTP;
+    }
+    
+    public String getNama() {
+        return nama;
+    }
+    
+    public String getNoKTP() {
+        return noKTP;
+    }
+    
+    @Override
+    public String toString() {
+        return "Nama: " + nama + ", No KTP: " + noKTP;
+    }
+    
+    public abstract double gaji(); {
+    }
+}
