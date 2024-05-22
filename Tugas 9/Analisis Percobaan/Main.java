3a9f4f4d6a6a0d8232535a97d16a69b5f697d044
nurfikaalfianingrum <160575631+nurfikaalfianingrum@users.noreply.github.com>
2024-05-22 19:55:27 +0700

    Main.java

---
+++ Tugas 9/Analisis Percobaan/Main.java
@@ -0,0 +1,9 @@
+package latihaninterface;
+
+public class Main {
+    public static void main(String[] args) {
+        Manusia manusia = new Manusia("Bambang", 19);
+        manusia.tampilkanNama();
+        manusia.tampilkanUmur();
+    }
+}
